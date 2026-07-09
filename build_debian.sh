03272bd2856889991d2a30471f6d0660c1a510a9
Dario Griffo <dariogriffo@gmail.com>
2026-07-09 15:31:14 +0100

    Use ~ instead of + for the distro release suffix

--- build_debian.sh
+++ build_debian.sh
@@ -61,7 +61,7 @@ build_architecture() {
     declare -a arr=("bookworm" "trixie" "forky" "sid")
 
     for dist in "${arr[@]}"; do
-        FULL_VERSION="$atuin_VERSION-${BUILD_VERSION}+${dist}_${build_arch}"
+        FULL_VERSION="$atuin_VERSION-${BUILD_VERSION}~${dist}_${build_arch}"
         echo "  Building $FULL_VERSION"
 
         if ! docker build . -t "atuin-$dist-$build_arch" \
--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -61,7 +61,7 @@ build_architecture() {
     declare -a arr=("jammy" "noble" "questing" "resolute")
 
     for dist in "${arr[@]}"; do
-        FULL_VERSION="$atuin_VERSION-${BUILD_VERSION}+${dist}_${build_arch}_ubu"
+        FULL_VERSION="$atuin_VERSION-${BUILD_VERSION}~${dist}_${build_arch}_ubu"
         echo "  Building $FULL_VERSION"
 
         if ! docker build . -f Dockerfile.ubu -t "atuin-ubuntu-$dist-$build_arch" \
